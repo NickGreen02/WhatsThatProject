fc147a9036d8200abc358e055ff7b579b27e3565
NickGreen02 <102611122+NickGreen02@users.noreply.github.com>
2023-03-27 16:15:13 +0100

    Add update username

--- components/yourProfile.js
+++ components/yourProfile.js
@@ -8,7 +8,13 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 export default class YourProfileApp extends Component {
   constructor(props) {
     super(props);
-    this.state = { userData: {} };
+    this.state = {
+      userData: {},
+      firstnamechange: '',
+      lastnamechange: '',
+      passwordchange: '',
+      emailchange: '',
+    };
   }
 
   componentDidMount() {
@@ -47,8 +53,63 @@ export default class YourProfileApp extends Component {
       });
   }
 
+  async changeFirstName() {
+    const { firstnamechange } = this.state;
+    const { navigation } = this.props;
+    const user = await AsyncStorage.getItem('whatsthat_user_id');
+    const urlTemplate = 'http://localhost:3333/api/1.0.0/user/';
+    const url = urlTemplate.concat(user);
+    return fetch(
+      url,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Authorization': await AsyncStorage.getItem('whatsthat_session_token'),
+        },
+        body: JSON.stringify({
+          first_name: firstnamechange,
+        }),
+      },
+    )
+      .then((response) => {
+        if (response.status === 200) {
+          navigation.navigate('ChatList');
+          return response.json();
+        } if (response.status === 401) {
+          throw new Error('Unauthorised access');
+        } if (response.status === 403) {
+          throw new Error('Forbidden by server');
+        } if (response.status === 404) {
+          throw new Error('Not found');
+        } if (response.status === 500) {
+          throw new Error('Server error');
+        } else {
+          throw new Error('Something went wrong');
+        }
+      })
+      .then((rJson) => {
+        console.log(rJson);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  changeLastName() {
+    console.log('change last name test');
+  }
+
+  changePassword() {
+    console.log('change password test');
+  }
+
+  changeEmail() {
+    console.log('change email address test');
+  }
+
   render() {
-    const { userData } = this.state;
+    const { userData, firstnamechange } = this.state;
     return (
       <View style={Styles.container}>
         <View style={Styles.formContainer}>
@@ -60,20 +121,31 @@ export default class YourProfileApp extends Component {
           </Text>
           <Text style={Styles.email}>{userData.email}</Text>
           <View style={Styles.updateUserContainer}>
-            <TextInput style={Styles.updateInput} placeholder="Change your first name" />
-            <TouchableOpacity>
+            <TextInput
+              style={Styles.updateInput}
+              placeholder="Change your first name"
+              onChangeText={(value) => { this.setState({ firstnamechange: value }); }}
+              value={firstnamechange}
+            />
+            <TouchableOpacity onPress={() => this.changeFirstName()}>
               <View style={Styles.updateButton}>
                 <Text style={Styles.buttonText}>Update</Text>
               </View>
             </TouchableOpacity>
             <TextInput style={Styles.updateInput} placeholder="Change your last name" />
-            <TouchableOpacity>
+            <TouchableOpacity onPress={() => this.changeLastName()}>
+              <View style={Styles.updateButton}>
+                <Text style={Styles.buttonText}>Update</Text>
+              </View>
+            </TouchableOpacity>
+            <TextInput style={Styles.updateInput} placeholder="Change your password" />
+            <TouchableOpacity onPress={() => this.changePassword()}>
               <View style={Styles.updateButton}>
                 <Text style={Styles.buttonText}>Update</Text>
               </View>
             </TouchableOpacity>
             <TextInput style={Styles.updateInput} placeholder="Change your email address" />
-            <TouchableOpacity>
+            <TouchableOpacity onPress={() => this.changeEmail()}>
               <View style={Styles.updateButton}>
                 <Text style={Styles.buttonText}>Update</Text>
               </View>
